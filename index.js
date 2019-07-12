b18933577955588fd215f8622edc24ff65a40bbd
Nimesh Subedi <sub17003@byui.edu>
2019-07-12 10:27:14 -0600

    pushing my node file

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 const express = require('express')
 const path = require('path')
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 8888
+const { Pool } = require('pg');
+const pool = new Pool({
+  connectionString: "postgres://postgres:Nimesh1998@localhost:5432/millionaire"
+});
 
 express()
   .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
+  .get('/getQuestion', (req, res) => {
+    pool.query("SELECT * FROM questions", function(error, data){
+      console.log("Error" + error);
+      res.json({result:data.rows})
+    })
+  })
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
